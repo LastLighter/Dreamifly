4bead47bf2be9d3313d6a2a0fd165eed5c09c9a6
amoorzheyu <szc8848@qq.com>
2025-11-29 02:19:45 +0800

    feat: add Z-Image-Turbo model configuration to GenerateForm component

--- src/components/GenerateForm.tsx
+++ src/components/GenerateForm.tsx
@@ -142,7 +142,8 @@ export default function GenerateForm({
         'Stable-Diffusion-3.5': 0.67,  // 40s/60s
         'Qwen-Image': 1.5,             // 48s/60s
         'Qwen-Image-Edit': 1.2,
-        'Wai-SDXL-V150': 0.1
+        'Wai-SDXL-V150': 0.1,
+        'Z-Image-Turbo': 0.325       // 20步1024*1024=13秒，换算到30步基准：13*(30/20)/60 = 0.325
       };
       
       const currentPixels = width * height;
